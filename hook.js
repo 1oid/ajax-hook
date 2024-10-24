47e20ce8358ac9c144572600073acd2ed7bae540
Loid <1398231927@qq.com>
2024-10-24 18:37:23 +0800

    feat: upload

---
+++ hook.js
@@ -0,0 +1,32 @@
+var _open = XMLHttpRequest.prototype.open;
+
+XMLHttpRequest.prototype.open = function(method, url) {
+    let _onreadystatechange = this.onreadystatechange;
+    let that = this;
+
+    this.addEventListener('readystatechange', function() {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+
+            // 如果想修改返回值，可以在这里修改
+            // let  response = that.responseText
+            // Object.defineProperty(that, 'responseText', {
+            //     value: response
+            // })
+        }
+        // call the original onreadystatechange
+        _onreadystatechange && _onreadystatechange.apply(this, arguments)
+    });
+
+    // when readyState change, call the original onreadystatechange
+    Object.defineProperty(this, "onreadystatechange", {
+        get() {
+            return _onreadystatechange
+        },
+        set(onreadystatechangeCallback) {
+            _onreadystatechange = onreadystatechangeCallback
+        }
+    })
+
+    return _open.apply(this, arguments);
+};
